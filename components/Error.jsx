841e5bf961d51a3c02a24f267c54270b08ec70e7
Lautaro López <llopez@folderit.net>
2022-03-04 12:39:13 -0300

    Feature create Error component

--- components/Error.jsx
+++ components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error = ({msg}) => {
     return ( 
-        <p className='p-3 bg-red-200 text-red-500 font-bold text-center'>
+        <p className='my-3 p-3 bg-red-200 text-red-500 font-bold text-center'>
             {msg}
         </p>
      );
