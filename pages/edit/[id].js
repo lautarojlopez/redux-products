9a858c34f0de0661354110db1976b9c26fb73f0c
Lautaro López <llopez@folderit.net>
2022-03-05 20:22:00 -0300

    Styles to edit page

--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import Layout from '../../components/Layout'
 import EditForm from '../../components/EditForm'
+import { useDispatch, useSelector } from 'react-redux'
+import { useRouter } from 'next/dist/client/router'
+import { getProductById } from '../../redux/actions/productsActions'
 
 const Edit = () => {
-    return ( 
+
+    const dispatch = useDispatch()
+
+    //Router object to get url query
+    const router = useRouter()
+
+    useEffect(() => {
+
+        //When router is ready, get product ID from query and call dispatch
+        if(router.isReady){
+            dispatch(getProductById(router.query.id))
+        }
+
+    }, [router.isReady])
+
+    return (
         <Layout>
-            <div className='m-auto w-6/12'>
-                <EditForm/>
+            <div className='m-auto w-11/12 lg:w-6/12 xl:w-5/12'>
+                <EditForm />
             </div>
         </Layout>
-     );
+    );
 }
- 
+
 export default Edit;
