7c0b35759727e9e36a2ca8f3738573b3bbc44d2c
Lautaro López <llopez@folderit.net>
2022-03-05 19:47:39 -0300

    Styles to ProductList component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,15 @@
 import React, {Fragment} from 'react'
 import Header from './Header';
-import Head from 'next/head';
+import Script from 'next/script';
 
 const Layout = ({children}) => {
     return ( 
         <Fragment>
-            <Head>
-			    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"/>
-            </Head>
             <Header/>
             {children}
+            <Script
+				src="https://kit.fontawesome.com/e323a62359.js"
+			/>
         </Fragment>
      );
 }
--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import Product from './Product';
+import { Fragment } from 'react';
 
-const ProductList = ({products}) => {
+const ProductList = ({ products }) => {
 
-    return ( 
-        <div className='p-5 m-auto w-11/12 rounded shadow-xl border border-gray-400'>
-            <h2 className='text-2xl text-center'>Products</h2>
-            {
-                products.length > 0 ? products.map((product) => (
-                    <Product 
-                        key={product.id} 
-                        product={product}
-                    />
-                )) : <p className=' my-3 text-center'>There are no products yet.</p>
-            }
-        </div>
-     );
+    return (
+        <Fragment>
+            <h2 className='text-4xl text-center my-5'>Products</h2>
+            <div className='m-auto w-11/12'>
+                {
+                    products.length > 0 ? products.map((product) => (
+                        <Product
+                            key={product.id}
+                            product={product}
+                        />
+                    )) : <p className='text-xl my-3 text-center'>There are no products yet.</p>
+                }
+            </div>
+        </Fragment>
+    );
 }
- 
+
 export default ProductList;
