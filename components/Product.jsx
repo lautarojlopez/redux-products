ad195ba6eec8af6bf0bd8822dda806f454527182
Lautaro López <llopez@folderit.net>
2022-03-05 22:14:07 -0300

    Styles for Product component

--- components/Product.jsx
+++ components/Product.jsx
@@ -14,13 +14,13 @@ const Product = ({product}) => {
     }
 
     return ( 
-        <div className='my-5 shadow rounded border border-gray-200 p-3'>
-            <p><span className='font-bold'>Name:</span> {name}</p>
-            <p><span className='font-bold'>Price:</span> ${price}</p>
-            <p><span className='font-bold'>Code:</span> {code}</p>
-            <p><span className='font-bold'>Description:</span> {description}</p>
+        <div className='my-5 shadow rounded border border-gray-200 p-5'>
+            <p><span className='font-bold text-indigo-700'>Name:</span> {name}</p>
+            <p><span className='font-bold text-indigo-700'>Price:</span> ${price}</p>
+            <p><span className='font-bold text-indigo-700'>Code:</span> {code}</p>
+            <p><span className='font-bold text-indigo-700'>Description:</span> {description}</p>
 
-            <div className='my-3'>
+            <div className='mt-3'>
                 <Link href={`/edit/${id}`}><a className='btn-edit mr-3'><i className="fas fa-edit"></i> Edit</a></Link>
                 <button onClick={() => deleteProduct(id)} className='btn-delete'><i className="fas fa-trash"></i> Delete</button>
             </div>
