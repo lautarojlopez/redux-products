7b93a6c5c79a773199e1c476250d098ea55b6b03
Lautaro López <llopez@folderit.net>
2022-03-05 22:09:07 -0300

    Styles to Product component

--- components/Product.jsx
+++ components/Product.jsx
@@ -14,7 +14,7 @@ const Product = ({product}) => {
     }
 
     return ( 
-        <div className='my-5 shadow rounded border border-gray-500 p-3'>
+        <div className='my-5 shadow rounded border border-gray-200 p-3'>
             <p><span className='font-bold'>Name:</span> {name}</p>
             <p><span className='font-bold'>Price:</span> ${price}</p>
             <p><span className='font-bold'>Code:</span> {code}</p>
