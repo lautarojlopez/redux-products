175abee99cda581eaf9ab42b0c437c3dac4ba200
Lautaro López <llopez@folderit.net>
2022-03-07 14:52:41 -0300

    Feature add Spinner while loading Products

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
-import Product from './Product';
-import { Fragment } from 'react';
+import Product from './Product'
+import { Fragment } from 'react'
+import { sortByName } from '../redux/actions/productsActions'
+import { useDispatch, useSelector } from 'react-redux'
+import Spinner from '../components/Spinner'
 
 const ProductList = ({ products }) => {
+
+    const dispatch = useDispatch()
+
+    //Get values from store
+    const loading = useSelector(state => state.products.loading)
+
+    const sortProductsByName = () => {
+        dispatch( sortByName() )
+    }
 
     return (
         <Fragment>
             <h2 className='text-4xl text-center my-5 text-orange-500 font-bold'>Products</h2>
+
+            <div className='flex m-auto justify-center items-center'>
+                <button onClick={() => sortProductsByName()} className='btn mr-3'>Name <i className="fas fa-arrow-up text-lg"></i></button>
+                <button className='btn mr-3'>Price <i className="fas fa-arrow-up text-lg"></i></button>
+                <button className='btn'>Code <i className="fas fa-arrow-up text-lg"></i></button>
+            </div>
+
             <div className='m-auto w-11/12'>
                 {
-                    products.length > 0 ? products.map((product) => (
-                        <Product
-                            key={product.id}
-                            product={product}
-                        />
-                    )) : <p className='text-xl my-3 text-center'>There are no products yet.</p>
+                    loading ? <div className='my-10'><Spinner/></div> : (
+                        products.length > 0 ? products.map((product) => (
+                            <Product
+                                key={product.id}
+                                product={product}
+                            />
+                        )) : <p className='text-xl my-3 text-center'>There are no products yet.</p>
+                    )
                 }
             </div>
         </Fragment>
-    );
+    )
 }
 
-export default ProductList;
+export default ProductList
