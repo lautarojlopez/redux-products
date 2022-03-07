3f931927b289357317947b4bd22403ac4daea9b5
Lautaro López <llopez@folderit.net>
2022-03-07 10:23:44 -0300

    Feature add displayName of current user to Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
+import { app } from '../config/firebase'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const Header = () => {
-    return ( 
+
+    const auth = getAuth()
+    const currentUser = auth.currentUser
+
+    return (
         <nav className='p-5 bg-indigo-800 text-white flex justify-between items-center'>
             <Link href="/"><h1 className='cursor-pointer text-3xl font-bold'>Redux</h1></Link>
-            <Link href="/add"><a className='btn'>Add Product <i className="fas fa-plus-circle"></i></a></Link>
+            <div className='flex justify-between items-center'>
+                {
+                    currentUser ? <p className='font-bold text-xl mr-5'>Welcome, {currentUser.displayName}</p> : null
+                }
+                <Link href="/add"><a className='btn'>Add Product <i className="fas fa-plus-circle"></i></a></Link>
+            </div>
         </nav>
-     );
+    );
 }
- 
+
 export default Header;
