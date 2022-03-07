2ca26597fc49fbf119d7f6b1e2b0f41fa1b4717c
Lautaro López <llopez@folderit.net>
2022-03-07 13:15:48 -0300

    Styles responsive design to Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,14 +16,14 @@ const Header = () => {
     }
 
     return (
-        <nav className='p-5 bg-indigo-800 text-white flex justify-between items-center'>
+        <nav className='p-5 bg-indigo-800 text-white flex flex-col md:flex-row justify-between items-center'>
             <Link href="/"><h1 className='cursor-pointer text-3xl font-bold'>Redux</h1></Link>
-            <div className='flex justify-between items-center'>
+            <div className='flex flex-col md:flex-row justify-between items-center'>
                 {
-                    currentUser ? <p className='font-bold text-xl mr-5'>Welcome, {currentUser.displayName}</p> : null
+                    currentUser ? <p className='font-bold text-xl mt-3 md:mr-5'>Welcome, {currentUser.displayName}</p> : null
                 }
-                <Link href="/add"><a className='btn'>Add Product <i className="fas fa-plus-circle"></i></a></Link>
-                <button onClick={() => logOut(auth)} className='btn-delete ml-5'>Log Out <i className="fa fa-sign-out"></i></button>
+                <Link href="/add"><a className='btn mt-3'>Add Product <i className="fas fa-plus-circle"></i></a></Link>
+                <button onClick={() => logOut(auth)} className='btn-delete mt-3 md:ml-5'>Log Out <i className="fa fa-sign-out"></i></button>
             </div>
         </nav>
     );
