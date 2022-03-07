1c8275769afcf8bdc1d24b4ed897ac11f1aee897
Lautaro López <llopez@folderit.net>
2022-03-07 11:26:51 -0300

    Refactor add unique ID to new products

--- components/AddForm.jsx
+++ components/AddForm.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { useFormik, yupToFormErrors } from "formik";
+import React from "react"
+import { useFormik } from "formik"
 import * as yup from 'yup'
-import { useDispatch, useSelector } from "react-redux";
-import Error from "./Error";
+import { useDispatch, useSelector } from "react-redux"
+import Error from "./Error"
+import { app } from "../config/firebase"
+import { getAuth } from "firebase/auth"
+import Spinner from "./Spinner"
+import shortid from "shortid"
 
 //Redux actions
-import { addProductAction } from "../redux/actions/productsActions";
+import { addProductAction } from "../redux/actions/productsActions"
 
 const AddForm = () => {
 
@@ -14,6 +18,11 @@ const AddForm = () => {
 
     //State values
     const error = useSelector(state => state.products.error)
+    const loading = useSelector(state => state.products.loading)
+
+    //Get current user
+    const auth = getAuth()
+    const currentUser = auth.currentUser
 
 
     const form = useFormik({
@@ -29,15 +38,17 @@ const AddForm = () => {
             code: yup.string().required("Add a code"),
             description: yup.string().required("Add a description")
         }),
-        onSubmit: (values) => {
-            dispatch( addProductAction(values) )
+        onSubmit: (product) => {
+            product.user = currentUser.uid
+            product.id = shortid.generate()
+            dispatch(addProductAction(product))
         }
-    });
+    })
 
     return (
         <form onSubmit={form.handleSubmit} className="flex flex-col my-5 shadow-lg rounded border border-gray-200 p-5">
             <h2 className="text-center text-2xl text-orange-500 font-bold">Add New Product</h2>
-            {error ? <Error msg="An error has occurred. Please try again."/> : null}
+            {error ? <Error msg="An error has occurred. Please try again." /> : null}
             <div className="my-5">
                 <div className="field">
                     <label htmlFor="name">Name</label>
@@ -47,7 +58,7 @@ const AddForm = () => {
                         value={form.values.name}
                         onChange={form.handleChange}
                     />
-                    {form.errors.name && form.touched.name ? <Error msg={form.errors.name}/> : null}
+                    {form.errors.name && form.touched.name ? <Error msg={form.errors.name} /> : null}
                 </div>
 
                 <div className="field">
@@ -58,7 +69,7 @@ const AddForm = () => {
                         value={form.values.price}
                         onChange={form.handleChange}
                     />
-                    {form.errors.price && form.touched.price ? <Error msg={form.errors.price}/> : null}
+                    {form.errors.price && form.touched.price ? <Error msg={form.errors.price} /> : null}
                 </div>
 
                 <div className="field">
@@ -69,7 +80,7 @@ const AddForm = () => {
                         value={form.values.code}
                         onChange={form.handleChange}
                     />
-                    {form.errors.code && form.touched.code ? <Error msg={form.errors.code}/> : null}
+                    {form.errors.code && form.touched.code ? <Error msg={form.errors.code} /> : null}
                 </div>
 
                 <div className="field">
@@ -82,15 +93,18 @@ const AddForm = () => {
                         value={form.values.description}
                         onChange={form.handleChange}
                     ></textarea>
-                    {form.errors.description && form.touched.description ? <Error msg={form.errors.description}/> : null}
+                    {form.errors.description && form.touched.description ? <Error msg={form.errors.description} /> : null}
                 </div>
             </div>
 
-            <button type="submit" className="btn w-10/12 m-auto">
-                Add <i className="fas fa-plus-circle"></i>
-            </button>
-        </form>
-    );
-};
+            {
+                loading ? <Spinner /> : <button type="submit" className="btn w-10/12 m-auto">
+                    Add <i className="fas fa-plus-circle"></i>
+                </button>
+            }
 
-export default AddForm;
+        </form>
+    )
+}
+
+export default AddForm
