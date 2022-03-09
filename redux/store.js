a8efef07e08c51a0db0aeaceae88199c87cc1559
Lautaro López <llopez@folderit.net>
2022-03-09 13:07:25 -0300

    Refactor migrate from redux createStore to redux-toolkit configureStore

--- redux/store.js
+++ redux/store.js
@@ -1,16 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import reducer from './reducers'
 import { configureStore } from '@reduxjs/toolkit'
+//Reducers
+import productsReducer from './reducers/productsReducer'
+import authReducer from './reducers/authReducer'
 
-//TODO: Import redux-toolkit and configure store
-
-const store = createStore(
-    reducer,
-    composeWithDevTools( 
-        applyMiddleware(thunk),
-    )
-)
+const store = configureStore({
+    reducer:{
+        products: productsReducer,
+        auth: authReducer
+    }
+})
 
 export default store
